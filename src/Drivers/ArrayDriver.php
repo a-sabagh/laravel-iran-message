603e64e09a42d06a57567aa645b9ac744ef7e3df
a-sabagh <a.sabagh72@gmail.com>
2025-07-15 12:22:15 +0330

    default configuration

--- src/Drivers/ArrayDriver.php
+++ src/Drivers/ArrayDriver.php
@@ -7,4 +7,10 @@ use IRMessage\Contracts\Driver;
 class ArrayDriver implements Driver
 {
     
+    /**
+     * The collection of default configuration for array driver.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    protected Collection $defaults;
 }
