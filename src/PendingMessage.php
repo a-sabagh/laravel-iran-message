320e84b8b157fd825822dbd63dedef325bcb117a
a-sabagh <a.sabagh72@gmail.com>
2025-07-15 17:56:21 +0330

    pending message with test

--- src/PendingMessage.php
+++ src/PendingMessage.php
@@ -2,7 +2,76 @@
 
 namespace IRMessage;
 
+use IRMessage\Contracts\Factory;
+
 class PendingMessage
 {
-    
+    /**
+     * The message manager instance.
+     *
+     * @var \IRMessage\MessageManager
+     */
+    protected $manager;
+
+    /**
+     * The recipients array
+     *
+     * @var array
+     */
+    protected $recipients = [];
+
+    /**
+     * The "from" number
+     *
+     * @var string
+     */
+    public $from;
+
+    /**
+     * The "message" body;
+     *
+     * @var array
+     */
+    protected $message = [];
+
+    /**
+     * The "args" for compiling with message body.
+     *
+     * @var array
+     */
+    protected $args = [];
+
+    public function __construct(Factory $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function to($recipients): self
+    {
+        $this->recipients = is_array($recipients) ? $recipients : func_get_args();
+        return $this;
+    }
+
+    public function from(string $from): self
+    {
+        $this->from = $from;
+        return $this;
+    }
+
+    public function message(string $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function args(array $args): self
+    {
+        $this->args = $args;
+        return $this;
+    }
+
+    public function send(): mixed
+    {
+        return $this->manager->driver()->send($this->recipients, $this->message, $this->from, $this->args);
+    }
 }
---
+++ tests/PendingMessageTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use IRMessage\PendingMessage;
+use IRMessage\Contracts\Factory;
+use Orchestra\Testbench\TestCase;
+
+class PendingMessageTest extends TestCase
+{
+    public function test_pending_message_instanciate(): void
+    {
+        $recipients = [fake()->phoneNumber()];
+        $from = fake()->phoneNumber();
+        $message = fake()->sentence(3);
+        $args = fake()->words(3);
+
+        $messageManagerMock = $this
+            ->mock(
+                Factory::class,
+                fn($mock) => $mock
+                    ->shouldReceive('driver')
+                    ->andReturnSelf()
+                    ->shouldReceive('send')
+                    ->with($recipients, $message, $from, $args)
+                    ->andReturn(true)
+            );
+
+        $pendingMessage = new PendingMessage($messageManagerMock);
+
+        $pendingMessage
+            ->to($recipients)
+            ->from($from)
+            ->message($message)
+            ->args($args)
+            ->send();
+
+        $this->addToAssertionCount(1);
+    }
+}
