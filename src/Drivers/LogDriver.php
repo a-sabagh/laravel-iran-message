5f074609eeafe3899a6176b198a3d7eecd3d481e
a-sabagh <a.sabagh72@gmail.com>
2025-07-15 13:30:02 +0330

    make log driver stringable

--- src/Drivers/LogDriver.php
+++ src/Drivers/LogDriver.php
@@ -3,11 +3,17 @@
 namespace IRMessage\Drivers;
 
 use IRMessage\Contracts\Driver;
+use Stringable;
 
-class LogDriver implements Driver
+class LogDriver implements Driver, Stringable
 {
     public function send(array|string $recipients, string $message, array $args = [], string $from = null)
     {
         
+    }
+
+    public function __toString(): string
+    {
+        return 'Log';
     }
 }
