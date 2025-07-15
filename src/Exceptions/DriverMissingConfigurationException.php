718c77a6e9281d13a8a464ebfa988a193f3d1ee1
a-sabagh <a.sabagh72@gmail.com>
2025-07-15 13:35:23 +0330

    remove useless param

--- src/Exceptions/DriverMissingConfigurationException.php
+++ src/Exceptions/DriverMissingConfigurationException.php
@@ -7,12 +7,12 @@ use InvalidArgumentException;
 class DriverMissingConfigurationException extends InvalidArgumentException
 {
     /**
+     * Create a new exception for a missing driver configuration.
      *
      * @param  string  $driver
-     * @param  array<int, string>  $keys
      * @return static
      */
-    public static function make($driver, $keys)
+    public static function make($driver)
     {
         /** @phpstan-ignore new.static */
         return new static("Missing required configuration keys for [{$driver}] OAuth driver.");
