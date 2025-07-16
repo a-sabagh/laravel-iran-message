8c170d739a58b707efa393c3d42f400345e70624
a-sabagh <a.sabagh72@gmail.com>
2025-07-16 17:01:57 +0330

    make log driver translateable

--- src/Drivers/LogDriver.php
+++ src/Drivers/LogDriver.php
@@ -2,11 +2,14 @@
 
 namespace IRMessage\Drivers;
 
+use IRMessage\Concerns\TranslatableMessage;
 use IRMessage\Contracts\Driver;
 use Stringable;
 
 class LogDriver implements Driver, Stringable
 {
+    use TranslatableMessage;
+    
     public function send(array|string $recipients, string $message, array $args = [], string $from = null)
     {
         
