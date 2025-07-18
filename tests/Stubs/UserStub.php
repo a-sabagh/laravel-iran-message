0bd310194464939c6bc4a57c109dba79ec53fcbf
a-sabagh <a.sabagh72@gmail.com>
2025-07-18 22:30:59 +0330

    remove useless namespaces

--- tests/Stubs/UserStub.php
+++ tests/Stubs/UserStub.php
@@ -4,8 +4,6 @@ namespace IRMessage\Tests\Stubs;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as AuthUser;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Orchestra\Testbench\Factories\UserFactory;
 
 class UserStub extends AuthUser
 {
