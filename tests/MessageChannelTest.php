6bae3fe2dc57d3299109b3e889a0ad27683f1d0b
a-sabagh <a.sabagh72@gmail.com>
2025-07-18 19:56:55 +0330

    test message channel

---
+++ tests/MessageChannelTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests;
+
+use Mockery;
+use Orchestra\Testbench\TestCase;
+use IRMessage\MessageServiceProvider;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Notifications\Dispatcher;
+use IRMessage\Channels\MessageChannel;
+
+class MessageChannelTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            MessageServiceProvider::class
+        ];
+    }
+
+    public function test_notification_message_channel_manager_extend(): void
+    {
+        $channel = $this->app->make(Dispatcher::class)->driver('message');
+
+        $this->assertInstanceOf(MessageChannel::class, $channel);
+    }
+}
