059222eaa64d897c3a6d5fad5cd83918dc92c797
a-sabagh <a.sabagh72@gmail.com>
2025-07-23 15:12:06 +0330

    throttle attempt concerns

---
+++ src/Concerns/ThrottleAttempt.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace IRMessage\Concerns;
+
+use Illuminate\Cache\RateLimiter;
+
+trait ThrottleAttempt
+{
+    protected function clearAttempts(int $phoneNumber, int $countryCode = 98)
+    {
+        $this->limiter()->clear($this->throttleKey($phoneNumber, $countryCode));
+    }
+
+    protected function throttleKey(int $phoneNumber, int $countryCode = 98)
+    {
+        return "otp:{$countryCode}:{$phoneNumber}";
+    }
+
+    protected function limiter()
+    {
+        return app(RateLimiter::class);
+    }
+
+    public function maxAttempts()
+    {
+        return property_exists($this, 'maxAttempts') ? $this->maxAttempts : 5;
+    }
+
+    public function decayMinutes()
+    {
+        return property_exists($this, 'decayMinutes') ? $this->decayMinutes : 1;
+    }
+}
