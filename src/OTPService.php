49a802db24fad4f842fc13a8fa2814ff5d78ce61
a-sabagh <a.sabagh72@gmail.com>
2025-07-24 17:02:37 +0330

    customize otp message params

--- src/OTPService.php
+++ src/OTPService.php
@@ -6,17 +6,47 @@ use IRMessage\Contracts\Factory;
 
 class OTPService
 {
+    public static $messageBodyCallback;
+
+    public static $messageArgsCallback;
+
     public function __construct(
-        protected Factory $message
+        protected Factory $messageManager
     ) {}
 
-    public function send()
-    {
-
-    }
+    public function send() {}
 
     public function message(): Factory
     {
-        return $this->message;
+        return $this->messageManager;
+    }
+
+    public function getMessageBody(): string
+    {
+        if (is_callable(static::$messageBodyCallback)) {
+            return call_user_func(static::$messageBodyCallback);
+        }
+
+        return 'otp';
+    }
+
+    public function getMessageArgs(): array
+    {
+        if (is_callable(static::$messageArgsCallback)) {
+            return call_user_func(static::$messageArgsCallback);
+        }
+
+        $verificationCode = rand(9999, 1000);
+        return ['verification_code' => $verificationCode];
+    }
+
+    public static function messageBodyUsing($callback): void
+    {
+        static::$messageBodyCallback = $callback;
+    }
+
+    public static function messageArgsUsing($callback): void
+    {
+        static::$messageArgsCallback = $callback;
     }
 }
--- tests/Feature/OTPServiceTest.php
+++ tests/Feature/OTPServiceTest.php
@@ -2,14 +2,17 @@
 
 namespace IRMessage\Tests\Feature;
 
+use IRMessage\OTPService;
 use IRMessage\Facades\OTP;
 use IRMessage\MessageManager;
 use Orchestra\Testbench\TestCase;
 use IRMessage\MessageServiceProvider;
-use IRMessage\OTPService;
+use IRMessage\Concerns\ThrottleAttempt;
 
 class OTPServiceTest extends TestCase
 {
+    use ThrottleAttempt;
+
     protected function getPackageProviders($app): array
     {
         return [
@@ -20,7 +23,18 @@ class OTPServiceTest extends TestCase
     public function test_otp_service_facade(): void
     {
         $this->assertInstanceOf(OTPService::class, OTP::getFacadeRoot());
-        
+
         $this->assertInstanceOf(MessageManager::class, OTP::message());
+    }
+
+    public function test_otp_service_static_message_configuration(): void
+    {
+        $expectedMessageBody = 'one-time-password';
+
+        OTPService::messageBodyUsing(fn() => $expectedMessageBody);
+
+        $actualMessageBody = OTP::getMessageBody();
+
+        $this->assertEquals($expectedMessageBody, $actualMessageBody);
     }
 }
