b6675dd4775f978170c316cbebcdde79716226f2
a-sabagh <a.sabagh72@gmail.com>
2025-07-25 11:27:25 +0330

    exception messages language file

---
+++ lang/en/exceptions.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IRMessage Exception Messages
+    |--------------------------------------------------------------------------
+    |
+    | The Messages which will return from exceptions
+    |
+    */
+
+    'otp' => [
+        'throttle' => 'Too Many OTP Request attempt, the next attempt is available in :meniutes and :second'
+    ]
+];
