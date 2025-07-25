57c764b02fdd0cc4d46f21caae8aea87bb12d853
a-sabagh <a.sabagh72@gmail.com>
2025-07-25 11:25:19 +0330

    too many otp request exception

---
+++ src/Events/TooManyOTPRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IRMessage\Events;
+
+class TooManyOTPRequest
+{
+    public function __construct(
+        public string $countryCode,
+        public string $phoneNumber
+    ){}
+}
