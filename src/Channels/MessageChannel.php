97a68ea00fabf01095f85b76d03796ec3c85d14f
a-sabagh <a.sabagh72@gmail.com>
2025-07-27 16:10:51 +0330

    message channel send

--- src/Channels/MessageChannel.php
+++ src/Channels/MessageChannel.php
@@ -2,7 +2,17 @@
 
 namespace IRMessage\Channels;
 
+use Illuminate\Notifications\Notification;
+use IRMessage\Contracts\Factory;
+
 class MessageChannel
 {
-    public function send() {}
+    public function __construct(
+        public Factory $manager
+    ){}
+
+    public function send(object $notifiable, Notification $notification) 
+    {
+        $notification->toMassage()->send();
+    }
 }
