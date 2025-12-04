d0c158a890cafacbb641dfda4b6159edf347b09d
a-sabagh <a.sabagh72@gmail.com>
2025-12-04 23:03:50 +0330

    improve docblock

--- src/Facades/Message.php
+++ src/Facades/Message.php
@@ -5,6 +5,11 @@ namespace IRMessage\Facades;
 use Illuminate\Support\Facades\Facade;
 use IRMessage\Contracts\Factory;
 
+/**
+ * @method static driver(?string $name = null) Get a driver instance.
+ *
+ * @see IRMessage\MessageManager
+ */
 class Message extends Facade
 {
     protected static function getFacadeAccessor()
